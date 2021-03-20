6e0dc3ac97405b7a629583ebbdec705c387a48f2
Noah Carmichael-Hitsman <noah.hitsman@gmail.com>
2021-03-19 23:06:08 -0700

    Annotated

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+// We use the object-creator.js function to create the object database stored in swapiObject and import it here for search functionality;
 import swapiDatabase from './swapiObject.js'
 
-//SETTING UP ALL THE NEEDED VARIABLES
 
-const selectionDiv = document.querySelector('#selection-div');
+//Here we set up all the veriables we will need below;
 const categorySelect = document.querySelector('#category-select');
 const numberInput = document.querySelector('#number-input');
 const selectionButton = document.querySelector('#selection-button');
@@ -10,32 +10,91 @@ const infoDiv = document.querySelector('#info-display');
 const reset = document.querySelector('#reset');
 const back = document.querySelector('#back')
 const searchBar = document.querySelector('#search-bar')
-let category;
-let idNumber;
 
-//GRABS FIRST KEY OF LINKED OBJ, AND APPENDS LINK TO INFODIV
 
-const linkSet = async (key, link) => {
-    let first;
-    let obj = await fetch(link).then(res => res.json())
-    for (let key in obj) {
-        first = obj[key];
-        break;
-    }
-
-    let div = document.createElement('div');
-    let a = document.createElement('a');
-    a.setAttribute('href', link);
-    a.classList.add('link');
-    div.classList.add('text')
-    a.innerHTML = `${key}:  ${first}`;
-    div.appendChild(a);
-    infoDiv.appendChild(div);
+//We check if there is a current page in the local storage, if so, load it up. We end up using this to load almost everything;
+if (localStorage.getItem('newLink')) {
+    getResponse(localStorage.getItem('newLink'))
 }
 
-//SETS INTO FROM THE PASSED IN OBJS TO DIVS AND APPENS THEM TO INFODIV
 
-const setInfo = async (apiObj) => {
+//This is the listener attached to the Search button, it will listen for 1) a category and id number, or 2) an input in the search bar;
+selectionButton.addEventListener('click', async e => {
+    if (numberInput.value) {
+        infoDiv.innerHTML = ''
+        let category = categorySelect.value;
+        let idNumber = numberInput.value;
+        if (localStorage.getItem('backLink')) {
+            localStorage.setItem('backLink', [localStorage.getItem('backLink'), localStorage.getItem('newLink')])
+        } else {
+            localStorage.setItem('backLink', [localStorage.getItem('newLink')])
+        }
+        numberInput.value = ''
+        localStorage.setItem('newLink', `https://swapi.dev/api/${category}/${idNumber}`)
+        location.reload();
+    } else if (searchBar.value) {
+        infoDiv.innerHTML = ''
+        let key = searchBar.value;
+        key = key.toLowerCase().trim()
+        let url = swapiDatabase[key];
+        if (localStorage.getItem('backLink')) {
+            localStorage.setItem('backLink', [localStorage.getItem('backLink'), localStorage.getItem('newLink')])
+        } else {
+            localStorage.setItem('backLink', [localStorage.getItem('newLink')])
+        }
+        localStorage.setItem('newLink', url)
+        location.reload()
+    }
+})
+
+
+/*When an api url is loaded to the infoDiv, it will create links that lead to other api urls.
+This listener will watch to see if someone clicks one of thos links; */
+infoDiv.addEventListener('click', async e => {
+    if (e.target.classList.contains('link')) {
+        e.preventDefault()
+        if (localStorage.getItem('backLink')) {
+            localStorage.setItem('backLink', [localStorage.getItem('backLink'), localStorage.getItem('newLink')])
+        } else {
+            localStorage.setItem('backLink', [localStorage.getItem('newLink')])
+        }
+        localStorage.setItem('newLink', e.target.href)
+        location.reload()
+    }
+})
+
+
+//The listener attachted to the back button. Will take you back to your recent pages;
+back.addEventListener('click', e => {
+    if (localStorage.getItem('backLink')) {
+        infoDiv.innerHTML = ''
+        let backArr = localStorage.getItem('backLink').split(',');
+        let backLast = backArr[backArr.length - 1];
+        backArr.pop()
+        localStorage.setItem('backLink', backArr);
+        localStorage.setItem('newLink', backLast);
+        location.reload()
+    }
+})
+
+
+//Will reset the page and local storage;
+reset.addEventListener('click', e => {
+    localStorage.clear()
+    location.reload()
+})
+
+
+//The function that takes a link and sends a request for data to the server using fetch;
+async function getResponse(link) {
+    let response = await fetch(link).then(res => res.json())
+    localStorage.setItem('newLink', link)
+    setInfo(response);
+}
+
+
+//This function is responsable for filling the HTML with the required elements to present the passed in data;
+async function setInfo(apiObj) {
     if (apiObj.detail) {
         let div = document.createElement('div');
         div.innerHTML = 'There is no entry at this index.'
@@ -63,85 +122,22 @@ const setInfo = async (apiObj) => {
     }
 }
 
-//GETS REQUESTED OBJ FROM THE SERVER AND RUNS THE SETINFO FUNCTION WITH IT
 
-const getResponse = async link => {
-    let response = await fetch(link).then(res => res.json())
-    localStorage.setItem('newLink', link)
-    setInfo(response);
-}
-
-//LICKS FOR THE SUBMIT BUTTON TO BE CLICKED, TAKING INFO FROM INPUTS AND GRABS LINK, SENDING IT TO GETRESPONSE
-
-selectionButton.addEventListener('click', async e => {
-    if (numberInput.value) {
-        infoDiv.innerHTML = ''
-        category = categorySelect.value;
-        idNumber = numberInput.value;
-        if (localStorage.getItem('backLink')) {
-            localStorage.setItem('backLink', [localStorage.getItem('backLink'), localStorage.getItem('newLink')])
-        } else {
-            localStorage.setItem('backLink', [localStorage.getItem('newLink')])
-        }
-        numberInput.value = ''
-        localStorage.setItem('newLink', `https://swapi.dev/api/${category}/${idNumber}`)
-        location.reload();
-    } else if (searchBar.value) {
-        infoDiv.innerHTML = ''
-        let key = searchBar.value;
-        key = key.toLowerCase().trim()
-        let url = swapiDatabase[key];
-        if (localStorage.getItem('backLink')) {
-            localStorage.setItem('backLink', [localStorage.getItem('backLink'), localStorage.getItem('newLink')])
-        } else {
-            localStorage.setItem('backLink', [localStorage.getItem('newLink')])
-        }
-        localStorage.setItem('newLink', url)
-        location.reload()
+//We move into this function a key has a link for a value, and we take that link and hyperlink it;
+async function linkSet(key, link) {
+    let first;
+    let obj = await fetch(link).then(res => res.json())
+    for (let key in obj) {
+        first = obj[key];
+        break;
     }
-})
 
-//LISTENS FOR LINKS IN THE INFODIV TO BE CLICKED, TAKES THAT LINK AND REPLACES PAGE WITH RESPONSE OBJECT
-
-infoDiv.addEventListener('click', async e => {
-    if (e.target.classList.contains('link')) {
-        e.preventDefault()
-        if (localStorage.getItem('backLink')) {
-            localStorage.setItem('backLink', [localStorage.getItem('backLink'), localStorage.getItem('newLink')])
-        } else {
-            localStorage.setItem('backLink', [localStorage.getItem('newLink')])
-        }
-        localStorage.setItem('newLink', e.target.href)
-        location.reload()
-    }
-})
-
-//RESETS LOCALSTORAGE AND INFODIV ON CLICK
-
-reset.addEventListener('click', e => {
-    infoDiv.innerHTML = ''
-    localStorage.clear()
-    location.reload()
-})
-
-//LISTENS FOR A CLICK ON THE BACK BUTTON, REMOVING THE LAST ENTRY FROM THE LOCALSTORAGE AND CHANGING THE CURRENT PAGE TO IT
-
-back.addEventListener('click', e => {
-    if (localStorage.getItem('backLink')) {
-        infoDiv.innerHTML = ''
-        let backArr = localStorage.getItem('backLink').split(',');
-        let backLast = backArr[backArr.length - 1];
-        backArr.pop()
-        localStorage.setItem('backLink', backArr);
-        localStorage.setItem('newLink', backLast);
-        location.reload()
-    }
-})
-
-//IF THERE IS A NEW LINK IN LOCAL STORAGE, WILL FILL THE PAGE WITH IT.
-
-let newLink = localStorage.getItem('newLink')
-
-if (newLink) {
-    getResponse(newLink)
+    let div = document.createElement('div');
+    let a = document.createElement('a');
+    a.setAttribute('href', link);
+    a.classList.add('link');
+    div.classList.add('text')
+    a.innerHTML = `${key}:  ${first}`;
+    div.appendChild(a);
+    infoDiv.appendChild(div);
 }
